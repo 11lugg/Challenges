7cb26da0db4dc74459bd6a48e8a1c6482ad8db60
Jon Lugg <jon.lugg@Nology.local>
2019-07-21 12:27:47 +0100

    tug a pug complete

---
+++ tug of war game/test.js
@@ -0,0 +1,12 @@
+
+
+      let countP1 = document.getElementById("countP1");
+      let countP2 = document.getElementById("countP2");
+
+      const updateHTML = () => {
+        // countP1.innerHTML = "Score:" + " " + updateScore;
+        countP1.innerHTML = "Score:  hello player1!!" ;
+      };
+
+
+updateHTML();
